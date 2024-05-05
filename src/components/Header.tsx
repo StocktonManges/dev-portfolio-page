27cd9643192f5b27eef2661cabd72ac4f70cee26
StocktonManges <stocktonmanges@gmail.com>
2024-05-05 09:40:52 -0600

    Added photos, headshot, and dummy 'About Me' text and set up nav provider

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function Header() {
   return (
     <>
-      <header className="d-flex flex-column justify-content-center align-items-center bg-primary py-3 text-center">
+      <header className="d-flex flex-column justify-content-center align-items-center py-3 text-center text-white">
         <h2>Stockton Manges Developer Portfolio</h2>
         <div className="d-flex flex-column align-items-center text-center">
           <div>385 - 208 - 2732</div>

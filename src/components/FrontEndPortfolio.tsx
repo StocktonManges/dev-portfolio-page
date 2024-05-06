d2d6188df4f686205ba1e587ab5d613d51b12666
StocktonManges <stocktonmanges@gmail.com>
2024-05-06 13:28:53 -0600

    Installed Bootstrap package and replaced styles.css with styles.scss. Finished styling project cards.

--- src/components/FrontEndPortfolio.tsx
+++ src/components/FrontEndPortfolio.tsx
@@ -1,28 +1,45 @@
 import { frontEndPortfolio } from "../utils";
 
-export default function FrontEndPortfolio() {
+export default function FullstackPortfolio() {
   return (
-    <section>
-      <h1>Front End Portfolio</h1>
-      <div>
+    <section className="w-100 d-flex flex-column align-items-center">
+      <h1 className="mt-5">Front End Portfolio</h1>
+      <div className="w-50">
         {frontEndPortfolio.map(
           ({ title, website, techStack, shortDescription, github }) => (
-            <div key={title} className="border border-primary">
-              <h2>{title}</h2>
-              <a href={website} target="_blank">
-                Website Link
-              </a>
-              <div>
-                <a href={github} target="_blank">
-                  GitHub Repository
-                </a>
+            <div key={title} className="card bg-primary rounded-4 p-1 my-5">
+              <h2 className="card-header p-3 text-white w-fit-content m-auto">
+                {title}
+              </h2>
+              <div className="card-body rounded-bottom-4 bg-white w-100 d-flex flex-column align-items-center">
+                <ul className="list-group w-25 text-center">
+                  <li className="list-group-item fs-5 active">Tech Stack</li>
+                  {techStack.map((item) => (
+                    <li className="list-group-item" key={item}>
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+
+                <p className="w-75 text-center my-3">{shortDescription}</p>
+
+                <div className="my-3 d-flex justify-content-center w-100 text-center">
+                  <a
+                    className="w-10 link-underline link-underline-opacity-0 link-underline-opacity-100-hover"
+                    href={website}
+                    target="_blank"
+                  >
+                    Website
+                  </a>
+                  <a
+                    className="w-10 border-start border-primary link-underline link-underline-opacity-0 link-underline-opacity-100-hover"
+                    href={github}
+                    target="_blank"
+                  >
+                    GitHub
+                  </a>
+                </div>
               </div>
-              <ul>
-                {techStack.map((item) => (
-                  <li key={item}>{item}</li>
-                ))}
-              </ul>
-              <p>{shortDescription}</p>
             </div>
           )
         )}
--- src/components/FullstackPortfolio.tsx
+++ src/components/FullstackPortfolio.tsx
@@ -3,13 +3,39 @@ import { fullstackPortfolio } from "../utils";
 export default function FullstackPortfolio() {
   return (
     <section className="w-100 d-flex flex-column align-items-center">
-      <h1 className="my-5">Fullstack Portfolio</h1>
+      <h1 className="mt-5">Fullstack Portfolio</h1>
       <div className="w-50">
         {fullstackPortfolio.map(
           ({ title, website, techStack, shortDescription, github }) => (
-            <div key={title} className="bg-primary rounded-4 p-1">
-              <h2 className="p-3 text-white w-fit-content m-auto">{title}</h2>
-              <div className="rounded-bottom-4 bg-white w-100 d-flex flex-column align-items-center">
+            <div key={title} className="card bg-primary rounded-4 p-1 my-5">
+              <h2 className="card-header p-3 text-white w-fit-content m-auto">
+                {title}
+              </h2>
+              <div className="card-body rounded-bottom-4 bg-white w-100 d-flex flex-column align-items-center">
+                <h3>Tech Stack</h3>
+
+                <div className="d-flex justify-content-evenly w-75 my-3 w-100">
+                  <ul className="list-group w-25">
+                    <li className="list-group-item fs-5 active">Front End</li>
+                    {techStack.frontEnd.map((item) => (
+                      <li className="list-group-item" key={item}>
+                        {item}
+                      </li>
+                    ))}
+                  </ul>
+
+                  <ul className="list-group w-25">
+                    <li className="list-group-item active fs-5">Backend</li>
+                    {techStack.backend.map((item) => (
+                      <li className="list-group-item" key={item}>
+                        {item}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+
+                <p className="w-75 text-center my-3">{shortDescription}</p>
+
                 <div className="my-3 d-flex justify-content-center w-100 text-center">
                   <a
                     className="w-30 link-underline link-underline-opacity-0 link-underline-opacity-100-hover"
@@ -33,28 +59,6 @@ export default function FullstackPortfolio() {
                     Backend GitHub
                   </a>
                 </div>
-
-                <div className="d-flex justify-content-evenly w-75 my-3 w-100">
-                  <ul className="list-group w-25">
-                    <li className="list-group-item fs-5 active">Front End</li>
-                    {techStack.frontEnd.map((item) => (
-                      <li className="list-group-item" key={item}>
-                        {item}
-                      </li>
-                    ))}
-                  </ul>
-
-                  <ul className="list-group w-25">
-                    <li className="list-group-item active fs-5">Backend</li>
-                    {techStack.backend.map((item) => (
-                      <li className="list-group-item" key={item}>
-                        {item}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-
-                <p className="w-75 text-center my-3">{shortDescription}</p>
               </div>
             </div>
           )
